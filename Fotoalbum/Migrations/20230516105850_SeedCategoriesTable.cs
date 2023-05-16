53c530bc30d3c236c88673a3861af162404c7954
gab-che <gabriellachessa1@gmail.com>
2023-05-16 13:01:50 +0200

    seed categories table

---
+++ Fotoalbum/Migrations/20230516105850_SeedCategoriesTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Fotoalbum.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoriesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "id", "description", "name" },
+                values: new object[,]
+                {
+                    { 1, "Capture your adventures around the world.", "travel" },
+                    { 2, "Explore the beauty of the great outdoors.", "nature" },
+                    { 3, "Savor delicious dishes from various cuisines.", "food" },
+                    { 4, "Capture the essence and personality of individuals.", "portrait" },
+                    { 5, "Discover stunning buildings and structures.", "architecture" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 5);
+        }
+    }
+}
--- Fotoalbum/Seeder/ModelBuilderExtension.cs
+++ Fotoalbum/Seeder/ModelBuilderExtension.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Fotoalbum.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fotoalbum.Seeder
@@ -7,6 +8,24 @@ namespace Fotoalbum.Seeder
     {
         public static void Seed(this ModelBuilder modelBuilder)
         {
+            Dictionary<string, string> photoCategories = new()
+            {
+                { "travel", "Capture your adventures around the world." },
+                { "nature", "Explore the beauty of the great outdoors." },
+                { "food", "Savor delicious dishes from various cuisines." },
+                { "portrait", "Capture the essence and personality of individuals." },
+                { "architecture", "Discover stunning buildings and structures." }
+            };
+
+            for(int i = 0; i < photoCategories.Count; i++)
+            {
+                KeyValuePair<string, string> category = photoCategories.ElementAt(i);
+                modelBuilder.Entity<Category>()
+                    .HasData(
+                        new Category { Id = i + 1, Name = category.Key, Description = category.Value }
+                    );
+            }
+
             modelBuilder.Entity<IdentityRole>().HasData(
                 new IdentityRole { Id = "1", Name = "SUPERADMIN", NormalizedName = "SUPERADMIN", ConcurrencyStamp = "16/05/2023" },
                 new IdentityRole { Id = "2", Name = "ADMIN", NormalizedName = "ADMIN", ConcurrencyStamp = "16/05/2023" },

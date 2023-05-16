89d33eacdcbf1160c0ea0845649708127dbedfa9
gab-che <gabriellachessa1@gmail.com>
2023-05-16 12:06:10 +0200

    adds empty views, photo index

---
+++ Fotoalbum/Controllers/PhotoalbumController.cs
@@ -0,0 +1,23 @@
+﻿using Fotoalbum.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fotoalbum.Controllers
+{
+    [Authorize]
+    public class PhotoalbumController : Controller
+    {
+        private PhotoalbumContext _photoalbumContext;
+        public PhotoalbumController(PhotoalbumContext photoalbumContext) => _photoalbumContext = photoalbumContext;
+
+        public IActionResult Index()
+        {
+            List<PhotoEntry> photos = _photoalbumContext.PhotoEntries.Include(p => p.Categories).ToList();
+            if (photos.Count > 0)
+                return View(photos);
+            else
+                return View("NoPhotos");
+        }
+    }
+}

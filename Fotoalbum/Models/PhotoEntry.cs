5323d338634501e49ed2a5e58e1071753d1d694e
gab-che <gabriellachessa1@gmail.com>
2023-05-16 17:34:03 +0200

    adds some basic form of validation

--- Fotoalbum/Models/PhotoEntry.cs
+++ Fotoalbum/Models/PhotoEntry.cs
@@ -7,13 +7,23 @@ namespace Fotoalbum.Models
     public class PhotoEntry
     {
         [Key][Column("id")] public int Id { get; set; }
-        [Column("title")] public string Title { get; set; }
-        [Column("description")] public string Description { get; set; }
+
+        [Required(ErrorMessage = "Campo obbligatorio")]
+        [Column("title")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Campo obbligatorio")]
+        [Column("description")]
+        public string Description { get; set; }
+
         [Column("is_visible")] public bool IsVisible { get; set; }
         public List<Category>? Categories { get; set; }
         [Column("image_id")] public int? ImageId { get; set; }
         public Image? Image { get; set; }
-        [NotMapped] public IFormFile? ImageFile { get; set; }
+
+        [Required(ErrorMessage = "Campo obbligatorio")]
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
         [NotMapped] public string ImageBase64 => Image == null ? "" : "data:image/jpg;base64," + Convert.ToBase64String(Image.Data);
 
         public PhotoEntry() { }
--- Fotoalbum/Models/PhotoFormModel.cs
+++ Fotoalbum/Models/PhotoFormModel.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Fotoalbum.Models
 {
@@ -6,6 +8,8 @@ namespace Fotoalbum.Models
     {
         public PhotoEntry PhotoEntry { get; set; }
         public List<SelectListItem>? Categories { get; set; }
+
+        [Required(ErrorMessage = "Campo obbligatorio")]
         public List<string>? SelectedCategories { get; set; }
     }
 }

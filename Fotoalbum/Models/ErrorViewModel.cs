86bacf4685b0151e0fae4039afb89721581907ae
gab-che <gabriellachessa1@gmail.com>
2023-05-16 09:33:26 +0200

    Add project files.

---
+++ Fotoalbum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fotoalbum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

3b6b1d36a752ed533eff05dd79270a97e1061e1a
gab-che <gabriellachessa1@gmail.com>
2023-05-17 16:25:48 +0200

    adds category index

---
+++ Fotoalbum/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Fotoalbum.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fotoalbum.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private PhotoalbumContext _photoalbumContext;
+        public CategoryController(PhotoalbumContext photoalbumContext) => _photoalbumContext = photoalbumContext;
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _photoalbumContext.Categories.ToList();
+            if(categories.Count > 0)
+                return View(categories);
+            else
+                return View("NoCategories");
+        }
+    }
+}

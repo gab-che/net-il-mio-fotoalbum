ae348cf56fa974bff2ebf65131af20f7372681ae
gab-che <gabriellachessa1@gmail.com>
2023-05-17 17:10:24 +0200

    adds category controller + views

--- Fotoalbum/Controllers/CategoryController.cs
+++ Fotoalbum/Controllers/CategoryController.cs
@@ -16,5 +16,70 @@ namespace Fotoalbum.Controllers
             else
                 return View("NoCategories");
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Category model = new();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category data)
+        {
+            Category model = new()
+            {
+                Name = data.Name,
+                Description = data.Description,
+            };
+            if (!ModelState.IsValid)
+                return View(model);
+            else
+            {
+                _photoalbumContext.Categories.Add(model);
+                _photoalbumContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Update(int Id)
+        {
+            try
+            {
+                Category cat = _photoalbumContext.Categories.FirstOrDefault(c => c.Id == Id);
+                return View(cat);
+            }
+            catch
+            {
+                return View("NotFound", Id);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int Id, Category data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(data);
+            }
+
+            try
+            {
+                Category catToEdit = _photoalbumContext.Categories.FirstOrDefault(c => c.Id == Id);
+                catToEdit.Name = data.Name;
+                catToEdit.Description = data.Description;
+                _photoalbumContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("NotFound", Id);
+            }
+
+
+        }
     }
 }
--- Fotoalbum/Models/Category.cs
+++ Fotoalbum/Models/Category.cs
@@ -7,8 +7,13 @@ namespace Fotoalbum.Models
     public class Category
     {
         [Key][Column("id")] public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obbligatorio")]
         [Column("name")] public string Name { get; set; }
+
+        [Required(ErrorMessage = "Campo obbligatorio")]
         [Column("description")] public string Description { get; set; }
+
         public List<PhotoEntry>? PhotoEntries { get; set; }
 
         public Category() { }
